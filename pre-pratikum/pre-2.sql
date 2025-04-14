a1f10e1c5b1883aa8eb103e100277c2b5505b9af
muhammadirawan <145947113+muhammadirawan@users.noreply.github.com>
2025-04-14 10:39:58 +0800

    Add files via upload

---
+++ pre-pratikum/pre-2.sql
@@ -0,0 +1,18 @@
+-- Nama : Muhammad Irawan
+-- NIM : 23241027
+-- KELAS : A
+
+-- melihat Database yang digunakan
+USE mysql;
+
+-- mendeskripsikan tabel user 
+DESCRIBE user;
+
+-- menampilkan tabel dalam data base mysql
+SHOW TABLES;
+
+-- mendeskripsikan tabel user
+DESCRIBE user;
+
+-- menampilkan isi data dari kolom Host,User,Password
+SELECT Host , User , Pasword FROM user;
